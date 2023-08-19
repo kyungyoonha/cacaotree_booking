536946d793ca0605f5a9b9fdce64038454c0b0b5
yoon <yoon@yoonui-MacBookPro.local>
2023-08-19 20:42:03 +0900

    feat: change home link to direct

--- src/views/ViewHome/feature.tsx
+++ src/views/ViewHome/feature.tsx
@@ -23,7 +23,7 @@ export const itemList = [
       </p>
     ),
     description: "막탄 내 원하는 곳에서 픽업 및 드랍이 가능한 패키지입니다.",
-    href: "/daytime/daytime-massage",
+    href: "/daytime/daytime-massage-direct",
     imageList: [
       "https://github.com/kyungyoonha/TIL/assets/29701385/d98fa03c-ce54-440a-a827-5d2f7cf33e9a",
       "https://github.com/kyungyoonha/TIL/assets/29701385/50825cbe-86be-4200-81c4-5a675d4e55be",
@@ -40,7 +40,7 @@ export const itemList = [
     ),
     description:
       "마지막날 체크아웃 후 짐보관 및 공항드랍이 가능한 패키지입니다.",
-    href: "/lastday/lastday-massage",
+    href: "/lastday/lastday-massage-direct",
     imageList: [
       "https://github.com/kyungyoonha/TIL/assets/29701385/68813c7b-b53f-475c-84ec-b7056a5dc7cf",
       "https://github.com/kyungyoonha/TIL/assets/29701385/ee5fd9d5-c747-449f-9394-ff941e137f80",
