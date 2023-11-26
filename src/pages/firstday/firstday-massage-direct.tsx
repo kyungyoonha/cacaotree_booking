9d6e196f51cfae81fd4ce243ef7f26ad838ac257
yoon <yoon@yoonui-MacBookPro.local>
2023-11-26 21:00:08 +0900

    push

--- src/pages/firstday/firstday-massage-direct.tsx
+++ src/pages/firstday/firstday-massage-direct.tsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import LayoutBasic from "@components/LayoutBasic";
 import ViewFirstdayMassageDirect from "@views/ViewFirstdayMassageDirect";
+import axios from "axios";
 
 const FirstdayMassage = () => {
   return (
